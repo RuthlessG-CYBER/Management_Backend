e391d09b28c408da9d9c7783004965c8e7fa9519
RuthlessG-CYBER <pandasoumya604@gmail.com>
2025-12-03 11:24:11 +0530

    Management_Backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(cors({
     origin: [
-        "https://management-frontend-wheat.vercel.app"
+        "https://management-frontend-wheat.vercel.app",
         // "http://localhost:5173"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
